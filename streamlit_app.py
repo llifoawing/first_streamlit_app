fe4a941af47177ad3920f1a56f45b4a64a5483df
Laura Li Foa Wing <100870296+llifoawing@users.noreply.github.com>
2023-08-29 15:36:16 -0700

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # create fruit input
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
