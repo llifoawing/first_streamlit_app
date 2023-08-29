9bf7f26aa75b73fcbe57c89dce33c0bad5f547a9
Laura Li Foa Wing <100870296+llifoawing@users.noreply.github.com>
2023-08-29 15:24:51 -0700

    normalize json

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize json data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output json in tabular format
+streamlit.dataframe(fruityvice_normalized)
